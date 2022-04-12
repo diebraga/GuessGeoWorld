3f1e571bf22261867abaa093846099a18241e555
diebraga <diebraga0055@gmail.com>
2022-04-12 22:55:36 +0100

    [added] logic add country and most countries helper

--- components/WorldCountriesMap/index.tsx
+++ components/WorldCountriesMap/index.tsx
@@ -1,56 +1,85 @@
-import React, { memo } from "react";
+import { FormEvent, memo, useRef, useState } from "react";
 import {
   ZoomableGroup,
   ComposableMap,
   Geographies,
   Geography
 } from "react-simple-maps";
+import MAP from '../../map/geoUrl.json'
+import { findCountryHelper } from "../../utils/findCountriesHelper";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-const rounded = (num: number) => {
-  if (num > 1000000000) {
-    return Math.round(num / 100000000) / 10 + "Bn";
-  } else if (num > 1000000) {
-    return Math.round(num / 100000) / 10 + "M";
-  } else {
-    return Math.round(num / 100) / 10 + "K";
-  }
-};
+type FoundCountries = {
+  name: string
+  id: string
+}
 
 const WorldCountriesMap = () => {
+  const [country, setCountry] = useState('')
+
+  const countriesRef = useRef<string[]>([])
+  console.log(countriesRef)
+  const [foundCountries, setFoundCountries] = useState<FoundCountries[]>([])
+  console.log(foundCountries)
+  function addCountry(e: FormEvent) {
+    e.preventDefault()
+    if (findCountryHelper(country) !== "") {
+      setFoundCountries(prev => [...prev, { name: findCountryHelper(country), id: '_' + Math.random().toString(36).substr(2, 9) }])
+      alert(`A new country was found ${findCountryHelper(country)}`)
+      setCountry("")
+    }
+  }
+
   return (
     <>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map(geo => {
-                // const { NAME, POP_EST } = geo.properties;
-                // setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+      <form onSubmit={addCountry}>
+        <input
+          onChange={e => setCountry(e.target.value)}
+          value={country}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {foundCountries.map(country => {
+        return (
+          <p key={country.id}>{country.name}</p>
+        )
+      })}
 
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    style={{
-                      default: {
-                        fill: "#D6D6DA",
-                        outline: "none"
-                      },
-                      hover: {
-                        fill: "#F53",
-                        outline: "none"
-                      },
-                      pressed: {
-                        fill: "#E42",
-                        outline: "none"
-                      }
-                    }}
-                  />
-                )
-              })
+      <ComposableMap data-tip="">
+        <ZoomableGroup>
+          <Geographies geography={MAP}>
+            {({ geographies }) => {
+              countriesRef.current = (geographies.map(item => item.properties.NAME))
+              return (
+                geographies.map(geo => {
+                  const { NAME, POP_EST } = geo.properties as { NAME: string, POP_EST: string }
+                  // setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                  // console.log(NAME)
+
+                  const isCountryFound = foundCountries.some(country => country.name === NAME)
+
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      style={{
+                        default: {
+                          fill: isCountryFound ? "green" : "#D6D6DA",
+                          outline: "none"
+                        },
+                        hover: {
+                          fill: "green",
+                          outline: "none"
+                        },
+                        // pressed: {
+                        //   fill: "#E42",
+                        //   outline: "none"
+                        // }
+                      }}
+                    />
+                  )
+                })
+              )
+            }
             }
           </Geographies>
         </ZoomableGroup>
---
+++ utils/findCountriesHelper.ts
@@ -0,0 +1,519 @@
+export function findCountryHelper(countryName: string) {
+  const formattedCountryName = countryName.toLocaleLowerCase().replace(/\s/g, "")
+  if (formattedCountryName === "brazil") {
+    return "Brazil"
+  }
+  else if (formattedCountryName === "afghanistan") {
+    return "Afghanistan"
+  }
+  else if (formattedCountryName === "angola") {
+    return "Angola"
+  }
+  else if (formattedCountryName === "albania") {
+    return "Albania"
+  }
+  else if (formattedCountryName === "unitedarabemirates" || formattedCountryName === "emirates") {
+    return "United Arab Emirates"
+  }
+  else if (formattedCountryName === "argentina") {
+    return "Argentina"
+  }
+  else if (formattedCountryName === "armenia") {
+    return "Armenia"
+  }
+  else if (formattedCountryName === "antarctica") {
+    return "Antarctica"
+  }
+  else if (formattedCountryName === "antarctica") {
+    return "Antarctica"
+  }
+  else if (formattedCountryName === "australia") {
+    return "Australia"
+  }
+  else if (formattedCountryName === "austria") {
+    return "Austria"
+  }
+  else if (formattedCountryName === "azerbaijan") {
+    return "Azerbaijan"
+  }
+  else if (formattedCountryName === "burundi") {
+    return "Burundi"
+  }
+  else if (formattedCountryName === "belgium") {
+    return "Belgium"
+  }
+  else if (formattedCountryName === "benin") {
+    return "Benin"
+  }
+  else if (formattedCountryName === "burkinafaso") {
+    return "Burkina Faso"
+  }
+  else if (formattedCountryName === "bangladesh") {
+    return "Bangladesh"
+  }
+  else if (formattedCountryName === "bulgaria") {
+    return "Bulgaria"
+  }
+  else if (formattedCountryName === "bahamas") {
+    return "Bahamas"
+  }
+  else if (formattedCountryName === "bosniaandherz." || formattedCountryName === "bosnia" || formattedCountryName === "bosniaandherzegovina") {
+    return "Bosnia and Herz."
+  }
+  else if (formattedCountryName === "belarus") {
+    return "Belarus"
+  }
+  else if (formattedCountryName === "belize") {
+    return "Belize"
+  }
+  else if (formattedCountryName === "bolivia") {
+    return "Bolivia"
+  }
+  else if (formattedCountryName === "brunei") {
+    return "Brunei"
+  }
+  else if (formattedCountryName === "bhutan") {
+    return "Bhutan"
+  }
+  else if (formattedCountryName === "botswana") {
+    return "Botswana"
+  }
+  else if (formattedCountryName === "centralafricanrep." || formattedCountryName === "centralafricanrepublic") {
+    return "Central African Rep."
+  }
+  else if (formattedCountryName === "canada") {
+    return "Canada"
+  }
+  else if (formattedCountryName === "switzerland") {
+    return "Switzerland"
+  }
+  else if (formattedCountryName === "chile") {
+    return "Chile"
+  }
+  else if (formattedCountryName === "Cameroon") {
+    return "Cameroon"
+  }
+  else if (formattedCountryName === "china") {
+    return "China"
+  }
+  else if (formattedCountryName === "côted'ivoire" || formattedCountryName === "coted'ivoire") {
+    return "Côte d'Ivoire"
+  }
+  else if (formattedCountryName === "dem.rep.congo" || formattedCountryName === "democraticrepublicofthecongo") {
+    return "Dem. Rep. Congo"
+  }
+  else if (formattedCountryName === "congo") {
+    return "Congo"
+  }
+  else if (formattedCountryName === "colombia") {
+    return "Colombia"
+  }
+  else if (formattedCountryName === "costarica") {
+    return "Costa Rica"
+  }
+  else if (formattedCountryName === "cuba") {
+    return "Cuba"
+  }
+  else if (formattedCountryName === "cyprus") {
+    return "Cyprus"
+  }
+  else if (formattedCountryName === "czechia") {
+    return "Czechia"
+  }
+  else if (formattedCountryName === "germany") {
+    return "Germany"
+  }
+  else if (formattedCountryName === "djibouti") {
+    return "Djibouti"
+  }
+  else if (formattedCountryName === "denmark") {
+    return "Denmark"
+  }
+  else if (formattedCountryName === "dominicanrep." || formattedCountryName === "dominicanrepublic") {
+    return "Dominican Rep."
+  }
+  else if (formattedCountryName === "algeria") {
+    return "Algeria"
+  }
+  else if (formattedCountryName === "ecuador") {
+    return "Ecuador"
+  }
+  else if (formattedCountryName === "egypt") {
+    return "Egypt"
+  }
+  else if (formattedCountryName === "eritrea") {
+    return "Eritrea"
+  }
+  else if (formattedCountryName === "Spain") {
+    return "Spain"
+  }
+  else if (formattedCountryName === "estonia") {
+    return "Estonia"
+  }
+  else if (formattedCountryName === "ethiopia") {
+    return "Ethiopia"
+  }
+  else if (formattedCountryName === "finland") {
+    return "Finland"
+  }
+  else if (formattedCountryName === "fiji") {
+    return "Fiji"
+  }
+  else if (formattedCountryName === "france") {
+    return "France"
+  }
+  else if (formattedCountryName === "gabon") {
+    return "Gabon"
+  }
+  else if (formattedCountryName === "georgia") {
+    return "Georgia"
+  }
+  else if (formattedCountryName === "ghana") {
+    return "Ghana"
+  }
+  else if (formattedCountryName === "guinea") {
+    return "Guinea"
+  }
+  else if (formattedCountryName === "unitedkingdom") {
+    return "United Kingdom"
+  }
+  else if (formattedCountryName === "gambia") {
+    return "Gambia"
+  }
+  else if (formattedCountryName === "guinea-bissau" || formattedCountryName === "guineabissau") {
+    return "Guinea-Bissau"
+  }
+  else if (formattedCountryName === "eq.guinea" || formattedCountryName === "equatorialguinea") {
+    return "Eq. Guinea"
+  }
+  else if (formattedCountryName === "greece") {
+    return "Greece"
+  }
+  else if (formattedCountryName === "greenland") {
+    return "Greenland"
+  }
+  else if (formattedCountryName === "guatemala") {
+    return "Guatemala"
+  }
+  else if (formattedCountryName === "guyana") {
+    return "Guyana"
+  }
+  else if (formattedCountryName === "honduras") {
+    return "Honduras"
+  }
+  else if (formattedCountryName === "Croatia") {
+    return "Croatia"
+  }
+  else if (formattedCountryName === "haiti") {
+    return "Haiti"
+  }
+  else if (formattedCountryName === "hungary") {
+    return "Hungary"
+  }
+  else if (formattedCountryName === "indonesia") {
+    return "Indonesia"
+  }
+  else if (formattedCountryName === "India") {
+    return "India"
+  }
+  else if (formattedCountryName === "ireland") {
+    return "Ireland"
+  }
+  else if (formattedCountryName === "iraq") {
+    return "Iraq"
+  }
+  else if (formattedCountryName === "iceland") {
+    return "Iceland"
+  }
+  else if (formattedCountryName === "israel") {
+    return "Israel"
+  }
+  else if (formattedCountryName === "italy") {
+    return "Italy"
+  }
+  else if (formattedCountryName === "jamaica") {
+    return "Jamaica"
+  }
+  else if (formattedCountryName === "jordan") {
+    return "Jordan"
+  }
+  else if (formattedCountryName === "japan") {
+    return "Japan"
+  }
+  else if (formattedCountryName === "kazakhstan") {
+    return "Kazakhstan"
+  }
+  else if (formattedCountryName === "kenya") {
+    return "Kenya"
+  }
+  else if (formattedCountryName === "kyrgyzstan") {
+    return "Kyrgyzstan"
+  }
+  else if (formattedCountryName === "cambodia") {
+    return "Cambodia"
+  }
+  else if (formattedCountryName === "southkorea") {
+    return "South Korea"
+  }
+  else if (formattedCountryName === "kosovo") {
+    return "Kosovo"
+  }
+  else if (formattedCountryName === "kuwait") {
+    return "Kuwait"
+  }
+  else if (formattedCountryName === "laos") {
+    return "Laos"
+  }
+  else if (formattedCountryName === "lebanon") {
+    return "Lebanon"
+  }
+  else if (formattedCountryName === "liberia") {
+    return "Liberia"
+  }
+  else if (formattedCountryName === "libya") {
+    return "Libya"
+  }
+  else if (formattedCountryName === "srilanka") {
+    return "Sri Lanka"
+  }
+  else if (formattedCountryName === "lesotho") {
+    return "Lesotho"
+  }
+  else if (formattedCountryName === "lithuania") {
+    return "Lithuania"
+  }
+  else if (formattedCountryName === "luxembourg") {
+    return "Luxembourg"
+  }
+  else if (formattedCountryName === "latvia") {
+    return "Latvia"
+  }
+  else if (formattedCountryName === "morocco") {
+    return "Morocco"
+  }
+  else if (formattedCountryName === "morocco") {
+    return "Morocco"
+  }
+  else if (formattedCountryName === "moldova") {
+    return "Moldova"
+  }
+  else if (formattedCountryName === "madagascar") {
+    return "Madagascar"
+  }
+  else if (formattedCountryName === "mexico") {
+    return "Mexico"
+  }
+  else if (formattedCountryName === "macedonia") {
+    return "Macedonia"
+  }
+  else if (formattedCountryName === "mali") {
+    return "Mali"
+  }
+  else if (formattedCountryName === "myanmar") {
+    return "Myanmar"
+  }
+  else if (formattedCountryName === "montenegro") {
+    return "Montenegro"
+  }
+  else if (formattedCountryName === "mongolia") {
+    return "Mongolia"
+  }
+  else if (formattedCountryName === "mozambique") {
+    return "Mozambique"
+  }
+  else if (formattedCountryName === "mauritania") {
+    return "Mauritania"
+  }
+  else if (formattedCountryName === "malawi") {
+    return "Malawi"
+  }
+  else if (formattedCountryName === "malaysia") {
+    return "Malaysia"
+  }
+  else if (formattedCountryName === "namibia") {
+    return "Namibia"
+  }
+  else if (formattedCountryName === "niger") {
+    return "Niger"
+  }
+  else if (formattedCountryName === "nigeria") {
+    return "Nigeria"
+  }
+  else if (formattedCountryName === "nicaragua") {
+    return "Nicaragua"
+  }
+  else if (formattedCountryName === "netherlands") {
+    return "Netherlands"
+  }
+  else if (formattedCountryName === "norway") {
+    return "Norway"
+  }
+  else if (formattedCountryName === "nepal") {
+    return "Nepal"
+  }
+  else if (formattedCountryName === "newzealand") {
+    return "New Zealand"
+  }
+  else if (formattedCountryName === "oman") {
+    return "Oman"
+  }
+  else if (formattedCountryName === "pakistan") {
+    return "Pakistan"
+  }
+  else if (formattedCountryName === "panama") {
+    return "Panama"
+  }
+  else if (formattedCountryName === "peru") {
+    return "Peru"
+  }
+  else if (formattedCountryName === "philippines") {
+    return "Philippines"
+  }
+  else if (formattedCountryName === "papuanewguinea") {
+    return "Papua New Guinea"
+  }
+  else if (formattedCountryName === "northkorea") {
+    return "North Korea"
+  }
+  else if (formattedCountryName === "poland") {
+    return "Poland"
+  }
+  else if (formattedCountryName === "portugal") {
+    return "Portugal"
+  }
+  else if (formattedCountryName === "paraguay") {
+    return "Paraguay"
+  }
+  else if (formattedCountryName === "palestine" || formattedCountryName === "stateofpalestine") {
+    return "Palestine"
+  }
+  else if (formattedCountryName === "qatar") {
+    return "Qatar"
+  }
+  else if (formattedCountryName === "romania") {
+    return "Romania"
+  }
+  else if (formattedCountryName === "russia") {
+    return "Russia"
+  }
+  else if (formattedCountryName === "rwanda") {
+    return "Rwanda"
+  }
+  else if (formattedCountryName === "saudiarabia") {
+    return "Saudi Arabia"
+  }
+  else if (formattedCountryName === "sudan") {
+    return "Sudan"
+  }
+  else if (formattedCountryName === "s.sudan" || formattedCountryName === "southsudan") {
+    return "S. Sudan"
+  }
+  else if (formattedCountryName === "senegal") {
+    return "Senegal"
+  }
+  else if (formattedCountryName === "solomonis." || formattedCountryName === "solomonislands") {
+    return "Solomon Is."
+  }
+  else if (formattedCountryName === "sierraleone") {
+    return "Sierra Leone"
+  }
+  else if (formattedCountryName === "elsalvador") {
+    return "El Salvador"
+  }
+  else if (formattedCountryName === "somalia") {
+    return "Somalia"
+  }
+  else if (formattedCountryName === "serbia") {
+    return "Serbia"
+  }
+  else if (formattedCountryName === "suriname") {
+    return "Suriname"
+  }
+  else if (formattedCountryName === "slovakia") {
+    return "Slovakia"
+  }
+  else if (formattedCountryName === "slovenia") {
+    return "Slovenia"
+  }
+  else if (formattedCountryName === "sweden") {
+    return "Sweden"
+  }
+  else if (formattedCountryName === "swaziland") {
+    return "Swaziland"
+  }
+  else if (formattedCountryName === "syria") {
+    return "Syria"
+  }
+  else if (formattedCountryName === "chad") {
+    return "Chad"
+  }
+  else if (formattedCountryName === "togo") {
+    return "Togo"
+  }
+  else if (formattedCountryName === "thailand") {
+    return "Thailand"
+  }
+  else if (formattedCountryName === "tajikistan") {
+    return "Tajikistan"
+  }
+  else if (formattedCountryName === "turkmenistan") {
+    return "Turkmenistan"
+  }
+  else if (formattedCountryName === "timor-leste" || formattedCountryName === "timorleste") {
+    return "Timor-leste"
+  }
+  else if (formattedCountryName === "trinidadandtobago") {
+    return "Trinidad and Tobago"
+  }
+  else if (formattedCountryName === "tunisia") {
+    return "Tunisia"
+  }
+  else if (formattedCountryName === "turkey") {
+    return "Turkey"
+  }
+  else if (formattedCountryName === "taiwan") {
+    return "Taiwan"
+  }
+  else if (formattedCountryName === "Uganda") {
+    return "Taiwan"
+  }
+  else if (formattedCountryName === "uganda") {
+    return "Uganda"
+  }
+  else if (formattedCountryName === "ukraine") {
+    return "Ukraine"
+  }
+  else if (formattedCountryName === "uruguay") {
+    return "Uruguay"
+  }
+  else if (formattedCountryName === "unitedstatesofamerica" || formattedCountryName === "unitedstates" || formattedCountryName === "usa") {
+    return "United States of America"
+  }
+  else if (formattedCountryName === "uzbekistan") {
+    return "Uzbekistan"
+  }
+  else if (formattedCountryName === "venezuela") {
+    return "Venezuela"
+  }
+  else if (formattedCountryName === "vietnam") {
+    return "Vietnam"
+  }
+  else if (formattedCountryName === "vanuatu") {
+    return "Vanuatu"
+  }
+  else if (formattedCountryName === "yemen") {
+    return "Yemen"
+  }
+  else if (formattedCountryName === "southafrica") {
+    return "South Africa"
+  }
+  else if (formattedCountryName === "zambia") {
+    return "Zambia"
+  }
+  else if (formattedCountryName === "zimbabwe") {
+    return "Zimbabwe"
+  }
+  else {
+    return ""
+  }
+}
