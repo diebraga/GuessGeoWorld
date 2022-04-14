0aba4a653bd0d0cdca44c512ebcdeec238d758b0
diebraga <diebraga0055@gmail.com>
2022-04-14 16:06:04 +0100

    [UP] styles modal completed countries

--- components/WorldCountriesMap/FoundAllCountriesModal.tsx
+++ components/WorldCountriesMap/FoundAllCountriesModal.tsx
@@ -4,15 +4,16 @@ import {
   ModalContent,
   ModalHeader,
   ModalFooter,
-  ModalBody,
   Button,
   Icon,
   Text,
   useColorModeValue,
+  Box,
 } from '@chakra-ui/react'
 import { Confetti } from '../Confetti'
 import { GiReturnArrow } from "react-icons/gi";
 import { AiFillHome } from "react-icons/ai";
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 type FoundAllCountriesModalProps = {
   isOpen: boolean
@@ -21,18 +22,49 @@ type FoundAllCountriesModalProps = {
 }
 
 export function FoundAllCountriesModal({ isOpen, onClose, onRestart }: FoundAllCountriesModalProps) {
+  const { width } = useWindowSize()
+
   return (
     <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} isCentered>
-      <Confetti isActive={true} />
       <ModalOverlay />
-      <ModalContent ml="4" mr="4">
-        <ModalHeader><Text as='span' color={useColorModeValue("blue.500", "blue.400")}>Congratulations!,</Text> {"You've completed the game!"} </ModalHeader>
-        <ModalFooter>
-          <Button onClick={onClose} colorScheme="red" mr={3} rightIcon={<Icon as={AiFillHome} />}>Home</Button>
-          <Button colorScheme='linkedin' rightIcon={<Icon as={GiReturnArrow} />} onClick={onRestart}>
-            Play again
-          </Button>
-        </ModalFooter>
+      <ModalContent h="100%" maxW="100%" bg="transparent">
+        <Confetti isActive={true} />
+        <Box
+          maxW="448px"
+          position="absolute"
+          top="50%"
+          left="50%"
+          marginRight="-50%"
+          style={{ transform: "translate(-50%, -50%)" }}
+        >
+          <Box
+            bg="white"
+            borderRadius='md'
+            ml="4"
+            mr="4"
+          >
+            <ModalHeader><Text as='span' color={useColorModeValue("blue.500", "blue.400")}>Congratulations!,</Text> {"You've completed the game!"} </ModalHeader>
+            <ModalFooter>
+              <Button
+                onClick={onClose}
+                colorScheme="red"
+                mr={3}
+                rightIcon={<Icon as={AiFillHome} />}
+                size={width as number < 400 ? "sm" : "md"}
+              >
+                Home
+              </Button>
+              <Button
+                colorScheme='linkedin'
+                rightIcon={<Icon as={GiReturnArrow} />}
+                onClick={onRestart}
+                size={width as number < 400 ? "sm" : "md"}
+              >
+                Play again
+              </Button>
+            </ModalFooter>
+          </Box>
+        </Box>
       </ModalContent>
     </Modal>
   )
