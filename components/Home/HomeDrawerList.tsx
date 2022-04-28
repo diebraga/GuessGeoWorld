19136f1a3ffc75bffebe484fb45896b291394e75
diebraga <diebraga0055@gmail.com>
2022-04-28 19:12:42 +0100

    [added] menu levels

--- components/Home/HomeDrawerList.tsx
+++ components/Home/HomeDrawerList.tsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   ListItem,
   UnorderedList,
   VStack,
@@ -12,17 +13,17 @@ export function HomeDrawerList() {
   const hrefLink = (router.query.path) as string
 
   const ListItems = [
-    { name: "World", param: "continent=world" },
-    { name: "Africa", param: "continent=africa" },
-    { name: "Asia", param: "continent=asia" },
-    { name: "North America", param: "continent=north-america" },
-    { name: "South America", param: "continent=south-america" },
-    { name: "Oceania", param: "continent=oceania" },
+    { name: "World", param: "continent=world", level: "hard", colorBadge: "red" },
+    { name: "Africa", param: "continent=africa", level: "medium", colorBadge: "yellow" },
+    { name: "Asia", param: "continent=asia", level: "medium", colorBadge: "yellow" },
+    { name: "North America", param: "continent=north-america", level: "medium-easy", colorBadge: "blue" },
+    { name: "South America", param: "continent=south-america", level: "easy", colorBadge: "green" },
+    { name: "Oceania", param: "continent=oceania", level: "easy", colorBadge: "green" },
   ]
 
   return (
     <UnorderedList as={VStack} listStyleType='none' textAlign='start' spacing='2' mt='4'>
-      {ListItems.map(({ name, param }) => {
+      {ListItems.map(({ name, param, colorBadge, level }) => {
         return (
           <ListItem
             key={name}
@@ -36,6 +37,9 @@ export function HomeDrawerList() {
             onClick={() => router.push(`/${hrefLink}?${param}`)}
           >
             {name}
+            <Badge ml='1' colorScheme={colorBadge} fontSize='x-small'>
+              {level}
+            </Badge>
           </ListItem>
         )
       })}
